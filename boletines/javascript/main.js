952f23483f5ee42065d8f70a597885b63ff6cab5
marroquinsamy <62960777+marroquinsamy@users.noreply.github.com>
2020-04-12 17:52:25 -0600

    Página terminada (excepción al demás contenido)

---
+++ boletines/javascript/main.js
@@ -0,0 +1,34 @@
+// Obtener los elementos de la clase .close
+let links = document.querySelectorAll(".close");
+// Recorrerlos
+links.forEach(function(link){
+    // Agregar un evento click a cada uno de ellos
+    link.addEventListener("click", function(ev){ // ev es el nombre más común para cuando se quiere obtener la lista de todos los eventos predeterminados de un elemento
+        ev.preventDefault();
+        // return false; Esta es otra forma de hacer lo de arriba
+        let content = document.querySelector(".content");
+        // Quitarle las clases de animación que ya tiene
+        content.classList.remove("bounceIn");
+        content.classList.remove("animated");
+        // Agregarle clases de animación de salida
+        content.classList.add("bounceOut");
+        content.classList.add("animated"); // Lo quito y lo vuelvo a poner para que repita el proceso de animación
+        setTimeout(function(){
+            location.href = "/";
+        }, 650);
+    });
+});
+// let verMas = document.querySelectorAll(".ver-mas");
+// verMas.forEach(function(link){
+//     link.addEventListener("click", function(ev){
+//         ev.preventDefault();
+//         let content = document.querySelector(".content");
+//         content.classList.remove("bounceIn");
+//         content.classList.remove("animated");
+//         content.classList.add("bounceOut");
+//         content.classList.add("animated");
+//         setTimeout(function(){
+//             location.href = 
+//         })
+//     })
+// })
